c188aef9f65174bd32b1357cb733d83ce897f76d
laurrain <tchebs.tchebayou1@gmail.com>
2015-05-06 12:44:09 +0200

    first push

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express")
+var exphbs = require("express-handlebars")
+  
+var app = express();
+app.engine("handlebars", exphbs({defaultLayout:"main"}))
+app.set("view engine", "handlebars")
+
+app.use("/static", express.static("views"))
+app.use("/static", express.static("."))
+
+app.get("/", function(req, res){
+	res.render("home")
+})
+
+app.get("/*", function(req, res){
+	res.render("home")
+})
+
+var server = app.listen(3000, function(){
+
+	console.log("server is running on " + server.address().address + ":" +server.address().port)
+
+})
